78c5aac100f2bcd08627764ad411a37600cd4d71
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-08-28 16:23:31 -0300

    Otro endpoint

--- src/main/kotlin/com/g7/server/EventoRoutes.kt
+++ src/main/kotlin/com/g7/server/EventoRoutes.kt
@@ -3,6 +3,7 @@ package com.g7.server
 import com.g7.evento.EventoDto
 import com.g7.evento.toDomain
 import com.g7.evento.toDto
+import com.g7.repo.EventoRepo
 import com.g7.repo.EventoRepository
 import com.g7.repo.UsuarioRepository
 import com.g7.usuario.toDto
@@ -10,6 +11,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import java.util.UUID
@@ -59,9 +61,9 @@ fun Route.eventoRoutes() {
     //usuarioId debería venir del contexto (jwt)
     post("eventos/{id}/inscriptos/{usuarioId}") {
         val id = call.requireUuidParam("id")?: return@post
+        val usuarioId = call.requireUuidParam("usuarioId")?: return@post
         EventoRepository.findById(id)
             .onSuccess { evento ->
-                val usuarioId = call.requireUuidParam("usuarioId")?: return@post
                 UsuarioRepository.getUsuarioFromId(usuarioId)
                     .onSuccess { usuario ->
                         evento.inscribir(usuario)
@@ -73,5 +75,23 @@ fun Route.eventoRoutes() {
                     .onFailure {
                         call.respond(HttpStatusCode.NotFound, it.message ?: "Unknown error") }
             }
-            .onFailure { call.respond(HttpStatusCode.NotFound, it.message ?: "Unknown error") }    }
+            .onFailure { call.respond(HttpStatusCode.NotFound, it.message ?: "Unknown error") }
+    }
+
+    delete ("eventos/{id}/inscriptos/{usuarioId}") {
+        val id = call.requireUuidParam("id")?: return@delete
+        val usuarioId = call.requireUuidParam("usuarioId")?: return@delete
+        EventoRepository.findById(id)
+            .onSuccess { evento ->
+                UsuarioRepository.getUsuarioFromId(usuarioId)
+                .onSuccess { usuario ->
+                    evento.cancelar(usuario)
+                        .onSuccess { call.respond(HttpStatusCode.OK) }
+                        .onFailure { call.respond(HttpStatusCode.BadRequest, it.message ?: "Unkown error") }
+                }
+                .onFailure { call.respond(HttpStatusCode.NotFound, it.message ?: "Unknown error") }
+            }
+        .onFailure { call.respond(HttpStatusCode.NotFound, it.message ?: "Unknown error") }
+    }
+
 }
