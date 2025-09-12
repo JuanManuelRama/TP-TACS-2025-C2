996ce544e51ac87121b45c7ac50c3c51a7897b49
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-09-12 17:52:17 -0300

    Layout updates with login

--- frontend/src/routes/+layout.tsx
+++ frontend/src/routes/+layout.tsx
@@ -4,6 +4,8 @@ import { Button } from "../components/ui/button";
 
 const Layout = ({ children }: { children?: ReactElement }) => {
 	const navigate = useNavigate();
+    const username = localStorage.getItem("username");
+    const isLoggedIn = !!username;
 	return (
 		<div className="h-screen">
 			<header className="h-14 border-b border-gray-200 flex flex-row items-center justify-start px-5">
@@ -11,9 +13,8 @@ const Layout = ({ children }: { children?: ReactElement }) => {
 				<Button
 					className="ml-auto"
 					variant="outline"
-					onClick={() => navigate("/auth")}
-				>
-					Log In
+                    onClick={() => navigate(isLoggedIn ? "/profile" : "/auth")}				>
+                    {isLoggedIn ? username : "Log In"}
 				</Button>
 			</header>
 			<main className="mt-10 mx-40">{children || <Outlet />}</main>
