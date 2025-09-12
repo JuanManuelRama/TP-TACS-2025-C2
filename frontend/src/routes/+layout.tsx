a6ac179f608ee63d31f448bf4151a9baf6e07ee2
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-09-12 18:00:00 -0300

    More complex layout

--- frontend/src/routes/+layout.tsx
+++ frontend/src/routes/+layout.tsx
@@ -3,23 +3,43 @@ import { Outlet, useNavigate } from "react-router";
 import { Button } from "../components/ui/button";
 
 const Layout = ({ children }: { children?: ReactElement }) => {
-	const navigate = useNavigate();
+    const navigate = useNavigate();
+
     const username = localStorage.getItem("username");
     const isLoggedIn = !!username;
-	return (
-		<div className="h-screen">
-			<header className="h-14 border-b border-gray-200 flex flex-row items-center justify-start px-5">
-				<span>Event Manager</span>
-				<Button
-					className="ml-auto"
-					variant="outline"
-                    onClick={() => navigate(isLoggedIn ? "/profile" : "/auth")}				>
-                    {isLoggedIn ? username : "Log In"}
-				</Button>
-			</header>
-			<main className="mt-10 mx-40">{children || <Outlet />}</main>
-		</div>
-	);
+
+    const handleLogout = () => {
+        localStorage.removeItem("jwt");
+        localStorage.removeItem("username");
+        window.location.reload();
+    };
+
+    return (
+        <div className="h-screen">
+            <header className="h-14 border-b border-gray-200 flex flex-row items-center justify-start px-5">
+                <span>Event Manager</span>
+
+                {isLoggedIn ? (
+                    <div className="ml-auto flex gap-2">
+                        <span>{username}</span>
+                        <Button variant="outline" onClick={handleLogout}>
+                            Logout
+                        </Button>
+                    </div>
+                ) : (
+                    <Button
+                        className="ml-auto"
+                        variant="outline"
+                        onClick={() => navigate("/auth")}
+                    >
+                        Log In
+                    </Button>
+                )}
+            </header>
+
+            <main className="mt-10 mx-40">{children || <Outlet />}</main>
+        </div>
+    );
 };
 
 export default Layout;
