602ad917b2937592bacea2da2cb469965fa63a7c
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-09-12 22:43:55 -0300

    include jwt

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -11,4 +11,12 @@ axiosInstance.interceptors.response.use(
     }
 )
 
+axiosInstance.interceptors.request.use((config) => {
+    const token = localStorage.getItem("jwt");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
 export default axiosInstance;
