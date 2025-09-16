c5d9a8a84b2c6c844b053499a93b144c5379ded1
Matias Martello <matias.martello@stonebranch.com>
2025-09-16 23:59:19 +0200

    fix: react-router+build

--- frontend/src/routes/auth/login/+page.tsx
+++ frontend/src/routes/auth/login/+page.tsx
@@ -1,73 +1,74 @@
-import { useState, FormEvent } from "react";
-import { useNavigate } from "react-router-dom";
-import "./+page.css";
 import { login } from "@/api/users.ts";
+import { type FormEvent, useState } from "react";
+import { useNavigate } from "react-router";
+import "./+page.css";
 
 const Page = () => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState<string | null>(null);
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState<string | null>(null);
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
 
-    async function handleLogin(e: FormEvent) {
-        e.preventDefault();
-        setError(null);
-        setLoading(true);
+	async function handleLogin(e: FormEvent) {
+		e.preventDefault();
+		setError(null);
+		setLoading(true);
 
-        try {
-            const data = await login(username.trim(), password.trim());
-            localStorage.setItem("jwt", data.token);
-            localStorage.setItem("username", data.user.username);
-            localStorage.setItem("id", data.user.id)
-            navigate("/events");
-        } catch (err: unknown) {
-            if (err instanceof Error) {
-                setError(err.message);
-            } else {
-                setError("Login failed, please try again");
-            }
-        } finally {
-            setLoading(false);
-        }
-    }
+		try {
+			const data = await login(username.trim(), password.trim());
+			localStorage.setItem("jwt", data.token);
+			localStorage.setItem("username", data.user.username);
+			localStorage.setItem("id", data.user.id);
+			navigate("/events");
+		} catch (err: unknown) {
+			if (err instanceof Error) {
+				setError(err.message);
+			} else {
+				setError("Login failed, please try again");
+			}
+		} finally {
+			setLoading(false);
+		}
+	}
 
-    return (
-        <form className="login-container" onSubmit={handleLogin}>
-            <h2>Log In</h2>
-            <div>
-                <label htmlFor="username">Username:</label>
-                <input
-                    id="username"
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    placeholder="Enter your username"
-                    disabled={loading}
-                />
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input
-                    id="password"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Enter your password"
-                    disabled={loading}
-                />
-            </div>
-            <button
-                type="submit"
-                disabled={loading}
-                className={`transition-opacity duration-300 ${
-                    loading ? "opacity-50 cursor-not-allowed" : "opacity-100"
-                }`}>
-                Log in
-            </button>
-            {error && <p style={{ color: "red" }}>{error}</p>}
-        </form>
-    );
+	return (
+		<form className="login-container" onSubmit={handleLogin}>
+			<h2>Log In</h2>
+			<div>
+				<label htmlFor="username">Username:</label>
+				<input
+					id="username"
+					type="text"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+					placeholder="Enter your username"
+					disabled={loading}
+				/>
+			</div>
+			<div>
+				<label htmlFor="password">Password:</label>
+				<input
+					id="password"
+					type="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					placeholder="Enter your password"
+					disabled={loading}
+				/>
+			</div>
+			<button
+				type="submit"
+				disabled={loading}
+				className={`transition-opacity duration-300 ${
+					loading ? "opacity-50 cursor-not-allowed" : "opacity-100"
+				}`}
+			>
+				Log in
+			</button>
+			{error && <p style={{ color: "red" }}>{error}</p>}
+		</form>
+	);
 };
 
 export { Page as Component };
--- frontend/src/routes/events/[id]/+actions.tsx
+++ frontend/src/routes/events/[id]/+actions.tsx
@@ -1,75 +1,77 @@
-import {Button} from "$components/ui/button.tsx";
-import {Event} from "@/types/event.ts";
-import {deleteEvent, subscribeEvent, unsubscribeEvent,} from "@/api/events.ts";
-import {useNavigate} from "react-router-dom";
-import {useSubscription} from "$routes/events/[id]/+aux.tsx";
-import {useState} from "react";
+import { Button } from "$components/ui/button.tsx";
+import { useSubscription } from "$routes/events/[id]/+aux.tsx";
+import { deleteEvent, subscribeEvent, unsubscribeEvent } from "@/api/events.ts";
+import type { Event } from "@/types/event.ts";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
-const Actions = ({isOwner, event}: {
-    isOwner: boolean;
-    event: Event;
-}) => {
-    const navigate = useNavigate();
-    const {isSubscribed, setSubscribed} = useSubscription(event)
-    const [message, setMessage] = useState<string | null>(null);
+const Actions = ({ isOwner, event }: { isOwner: boolean; event: Event }) => {
+	const navigate = useNavigate();
+	const { isSubscribed, setSubscribed } = useSubscription(event);
+	const [message, setMessage] = useState<string | null>(null);
 
-    const onSubscribe = async () => {
-        try {
-            if (!localStorage.getItem("id")) {
-                navigate("/auth/login");
-                return;
-            }
-            const result = await subscribeEvent(event.id);
-            setSubscribed(true);
-            if(result.tipo == "CONFIRMACION") setMessage("You have been confirmed for this event")
-            else setMessage("Unfortunately, there were no more spots left, you have been waitlisted")
-        } catch {
-            alert("Subscribe event failed");
-        }
-    }
+	const onSubscribe = async () => {
+		try {
+			if (!localStorage.getItem("id")) {
+				navigate("/auth/login");
+				return;
+			}
+			const result = await subscribeEvent(event.id);
+			setSubscribed(true);
+			if (result.tipo == "CONFIRMACION")
+				setMessage("You have been confirmed for this event");
+			else
+				setMessage(
+					"Unfortunately, there were no more spots left, you have been waitlisted",
+				);
+		} catch {
+			alert("Subscribe event failed");
+		}
+	};
 
-    const onUnsubscribe = async () => {
-        try {
-            await unsubscribeEvent(event.id);
-            setMessage("You have been unsubscribed for this event")
-            setSubscribed(false);
-        } catch {
-            alert("Unsubscribe event failed");
-        }
-    }
+	const onUnsubscribe = async () => {
+		try {
+			await unsubscribeEvent(event.id);
+			setMessage("You have been unsubscribed for this event");
+			setSubscribed(false);
+		} catch {
+			alert("Unsubscribe event failed");
+		}
+	};
 
-    const onDelete = async () => {
-        try {
-            await deleteEvent(event.id);
-            navigate("/events");
-        }
-        catch {
-            alert("Delete event failed");
-        }
-    }
+	const onDelete = async () => {
+		try {
+			await deleteEvent(event.id);
+			navigate("/events");
+		} catch {
+			alert("Delete event failed");
+		}
+	};
 
-    return (
-        <div className="flex flex-col items-end mr-10">
-            <div className="flex gap-2">
-                {isOwner ? (
-                    <Button variant="destructive" onClick={onDelete}>Eliminar</Button>
-                ) : isSubscribed ? (
-                    <Button onClick={onUnsubscribe}>Unsubscribe</Button>
-                ) : (
-                    <Button onClick={onSubscribe}>Subscribe</Button>
-                )}
-            </div>
+	return (
+		<div className="flex flex-col items-end mr-10">
+			<div className="flex gap-2">
+				{isOwner ? (
+					<Button variant="destructive" onClick={onDelete}>
+						Eliminar
+					</Button>
+				) : isSubscribed ? (
+					<Button onClick={onUnsubscribe}>Unsubscribe</Button>
+				) : (
+					<Button onClick={onSubscribe}>Subscribe</Button>
+				)}
+			</div>
 
-            {message && (
-                <div
-                    className="mt-2 px-3 py-1 rounded-md text-sm font-medium shadow-sm
-                    bg-gray-100 text-gray-800">
-                    {message}
-                </div>
-            )}
-        </div>
-    );
+			{message && (
+				<div
+					className="mt-2 px-3 py-1 rounded-md text-sm font-medium shadow-sm
+                    bg-gray-100 text-gray-800"
+				>
+					{message}
+				</div>
+			)}
+		</div>
+	);
 };
-
 
 export default Actions;
