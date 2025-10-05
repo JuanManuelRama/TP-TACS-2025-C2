04ec2fce5e81ca4432d4139271afa88b70a72ba0
JuanManuelRama <jrama@frba.utn.edu.ar>
2025-10-05 15:38:27 -0300

    server en ngixn

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -4,7 +4,7 @@ import HttpError from "./HttpError.ts";
 
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: "/api",
 })
 
 axiosInstance.interceptors.response.use(
